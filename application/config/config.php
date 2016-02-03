ef30feae7a1ccf839a82376e59c9a03df783fa8d
samo@stellarbluetechnologies.com <samo@stellarbluetechnologies.com>
2016-02-02 21:15:58 -0600

    attempting session fix

--- application/config/config.php
+++ application/config/config.php
@@ -355,10 +355,10 @@ $config['encryption_key'] = 'MSap5DuDVCXeNuur3fEUmBG0o2AvjNa9';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7776000;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
